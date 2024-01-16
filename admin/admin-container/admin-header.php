e24a25b89908589e2c90200ff50aba4546ed9919
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-17 00:10:14 +0545

    Add files via upload

---
+++ admin/admin-container/admin-header.php
@@ -0,0 +1,20 @@
+    <header>
+        <!-- <a href="#" class="logo"><img src="/home/linuxkali/Documents/My-VSCode/Vehicle-Rental-System-2/images/jeep.png" alt=""></a> -->
+        <a href="..\home.php" class="logo">Sadhan.com</a>
+
+       <div class="bx bx-menu" id="menu-icon"></div>
+        <ul class="navbar">
+            <li><a href="..\home.php">Home</a></li>
+            <!-- <li><a href="vehicles.php">Vehicles</a></li> -->
+            <!-- <li><a href="services.php">services</a></li> -->
+            <!-- <li><a href="reviews.php">Reviews</a></li> -->
+            <li><a href="..\contact.php">Contact</a></li>
+            <li><a href="..\about.php">About</a></li>
+        </ul>
+        <div class="header-btn">
+            <!-- <a href="#" class="sign-up">Sign Up</a> -->
+            <!-- <a href="sign-up.php" class="sign-in"><i class="fa-solid fa-user"></i> Sign Up</a> -->
+        </div>
+    </header>
+</body>
+</html>
