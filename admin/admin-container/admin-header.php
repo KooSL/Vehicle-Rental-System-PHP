92280ee96deb81c95836dd44cfe387fdf1c24d40
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-18 22:26:51 +0545

    Add files via upload

--- admin/admin-container/admin-header.php
+++ admin/admin-container/admin-header.php
@@ -1,20 +1,6 @@
-    <header>
-        <!-- <a href="#" class="logo"><img src="/home/linuxkali/Documents/My-VSCode/Vehicle-Rental-System-2/images/jeep.png" alt=""></a> -->
-        <a href="..\home.php" class="logo">Sadhan.com</a>
-
-       <div class="bx bx-menu" id="menu-icon"></div>
-        <ul class="navbar">
-            <li><a href="..\home.php">Home</a></li>
-            <!-- <li><a href="vehicles.php">Vehicles</a></li> -->
-            <!-- <li><a href="services.php">services</a></li> -->
-            <!-- <li><a href="reviews.php">Reviews</a></li> -->
-            <li><a href="..\contact.php">Contact</a></li>
-            <li><a href="..\about.php">About</a></li>
-        </ul>
-        <div class="header-btn">
-            <!-- <a href="#" class="sign-up">Sign Up</a> -->
-            <!-- <a href="sign-up.php" class="sign-in"><i class="fa-solid fa-user"></i> Sign Up</a> -->
-        </div>
-    </header>
-</body>
-</html>
+<header class="header">
+    <a href="..\home.php" class="logo"><i class="fa-solid fa-car"></i> Sadhan <i class="fa-solid fa-motorcycle"></i></a>
+    <div class="header-btn">
+        <a href="admin-login.php" class="sign-in"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
+    </div>
+</header>
---
+++ admin/admin-container/admin-leftbar.php
@@ -0,0 +1,22 @@
+<section class="leftbar">
+    <div class="leftbar-menu">
+    <nav>
+      <ul class="nav-item">
+        <!-- <li><a href="#">
+          <i class="fas fa-home"></i>
+          <span class="nav-item">Dashboard</span>
+        </a></li> -->
+        <li><a href="dashboard.php" class="nav-item"><i class="fa-solid fa-table-columns"></i> Dashboard</a></li>
+        <li><a href="bookings.php" class="nav-item"><i class="fa-solid fa-calendar"></i> Bookings</a></li>
+        <li><a href="vehicles.php" class="nav-item"><i class="fa-solid fa-car"></i> Vehicles</a></li>
+        <li><a href="brands.php" class="nav-item"><i class="fa-solid fa-car"></i> Brands</a></li>
+        <li><a href="customers.php" class="nav-item"><i class="fa-solid fa-user"></i> Customers</a></li>
+        
+        <!-- <li><a href="" class="logout">
+          <i class="fas fa-sign-out-alt"></i>
+          <span class="nav-item">Log out</span>
+        </a></li> -->
+      </ul>
+    </nav>
+    </div>
+</section>
