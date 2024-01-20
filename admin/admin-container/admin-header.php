78e0d8582acfb32f9d24e30791025aa54678a062
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-20 23:51:06 +0545

    Add files via upload

--- admin/admin-container/admin-header.php
+++ admin/admin-container/admin-header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <a href="..\home.php" class="logo"><i class="fa-solid fa-car"></i> Sadhan <i class="fa-solid fa-motorcycle"></i></a>
-    <div class="header-btn">
-        <a href="admin-login.php" class="sign-in"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
+    <div class="admin-header-btn">
+        <a href="admin-login.php" class="admin-logout"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
     </div>
 </header>
