34552d1dab1130285a99d05848a8cbeda7017ef6
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-20 23:50:40 +0545

    Add files via upload

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="..\css\style.css">
     <script src="https://kit.fontawesome.com/ae61999827.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <script src="..\js\main.js"></script>
     <title>Admin Login</title>
 </head>
 <body>
@@ -19,8 +20,12 @@
             <p class="login-txt"><i class="fa-solid fa-right-to-bracket"></i> Admin Log In</p>
             <div class="l-inputs">
             <input type="text" name="email" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
             </div>
+            <div class="admin-login-password-box">
+                <input type="password" name="password" placeholder="Password" id="adminloginpassword" required>
+                <i class="fa-solid fa-eye-slash" id="adminlogineyeicon" aria-hidden="true" onclicK="adminlogintoggle()"></i>
+            </div>
+            
             <p><a href="..\update-password.php" class="e-option">Forget Password?</a></p>
             <input type="submit" value="Login">
             <!-- <p><a href="sign-up.php" class="e-option">Create new account</a></p> -->
