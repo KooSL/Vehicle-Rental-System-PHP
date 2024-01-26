df288fa24f6924e2ccf1db8a513b37134e859144
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-26 23:39:49 +0545

    Add files via upload

---
+++ admin/admin-container/db_connection.php
@@ -0,0 +1,21 @@
+<?php 
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "vehicle-rental-system";
+
+//create connection
+$con = mysqli_connect($server,$username,$password,$database);
+
+//check connection
+// if($con)
+// {
+//     echo "Connection Successful";
+// }
+
+// else{
+//     echo "Connection Failed";
+// }
+
+?>
+
