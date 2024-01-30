962a9f12b44f5c15391aac94fd1a8ffd69e0bd2c
KooSL <142590197+KooSL@users.noreply.github.com>
2024-01-30 16:59:10 +0545

    Add files via upload

--- database/vehicle-rental-system.sql
+++ database/vehicle-rental-system.sql
@@ -1,1 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2024 at 12:08 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.0.32
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vehicle-rental-system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_login`
+--
+
+CREATE TABLE `admin_login` (
+  `a_id` int(11) NOT NULL,
+  `a_username` varchar(50) NOT NULL,
+  `a_password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_login`
+--
+
+INSERT INTO `admin_login` (`a_id`, `a_username`, `a_password`) VALUES
+(1, 'admin', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_signup`
+--
+
+CREATE TABLE `user_signup` (
+  `u_id` int(11) NOT NULL,
+  `u_name` varchar(50) NOT NULL,
+  `u_address` varchar(100) NOT NULL,
+  `u_email` varchar(50) NOT NULL,
+  `u_phone` varchar(15) NOT NULL,
+  `u_gender` varchar(20) NOT NULL,
+  `u_password` varchar(20) NOT NULL,
+  `u_reg_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_signup`
+--
+
+INSERT INTO `user_signup` (`u_id`, `u_name`, `u_address`, `u_email`, `u_phone`, `u_gender`, `u_password`, `u_reg_date`) VALUES
+(37, 'Kushal Acharya', 'Khairahani-13, Chitwan', 'dfdfd@gmail.com', '45764565', 'male', 'c20ad4d76fe97759aa27', '2024-01-26'),
+(38, 'Kushal Acharya', 'Khairahani-13, Chitwan', 'acharyakushal629@gmail.com', '9745355605', 'male', '202cb962ac59075b964b', '2024-01-26'),
+(39, 'ram bahadur', 'ktm', 'gf@gmail.com', '675767', 'other', '289dff07669d7a23de0e', '2024-01-26'),
+(40, 'gadu', 'ghar', 'dj@gmail.com', '000000', 'female', '202cb962ac59075b964b', '2024-01-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `v_id` int(11) NOT NULL,
+  `v_name` varchar(50) NOT NULL,
+  `v_type` varchar(50) NOT NULL,
+  `v_fuel` varchar(20) NOT NULL,
+  `v_number` varchar(30) DEFAULT NULL,
+  `v_cost` int(20) NOT NULL,
+  `v_image1` varchar(500) NOT NULL,
+  `v_image2` varchar(500) NOT NULL,
+  `v_image3` varchar(500) NOT NULL,
+  `v_description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`v_id`, `v_name`, `v_type`, `v_fuel`, `v_number`, `v_cost`, `v_image1`, `v_image2`, `v_image3`, `v_description`) VALUES
+(3, 'dgfd', 'suzuki', 'petrol', '45454', 45645, '_d6719bd0-ea6e-48e1-9b56-6aba814e0ca6.jpg', '', '', '44444444444'),
+(4, 'dio', 'tata', 'petrol', '434343', 20000, 'image-4.jpg', '', '', 'ghuiiiiiiii'),
+(5, 'Tesla', 'Car', 'ev', '34333', 50000, 'image-3.jpg', '', '', 'This is a tesla car');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  ADD PRIMARY KEY (`a_id`);
+
+--
+-- Indexes for table `user_signup`
+--
+ALTER TABLE `user_signup`
+  ADD PRIMARY KEY (`u_id`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `u_email` (`u_email`),
+  ADD UNIQUE KEY `u_phone` (`u_phone`);
+
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`v_id`),
+  ADD UNIQUE KEY `v_id` (`v_id`),
+  ADD UNIQUE KEY `v_number` (`v_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_login`
+--
+ALTER TABLE `admin_login`
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_signup`
+--
+ALTER TABLE `user_signup`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
